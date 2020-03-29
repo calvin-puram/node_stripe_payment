c5dc14fa22cd68612c5ed0cb3b0f549fc4f61269
calvin <puram.calvin@gmail.com>
2020-03-29 12:10:11 +0100

    change nodejs entrypoint

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require("express");
+
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
-const appRoute = require("./routes");
+const appRoute = require("./routes/routes");
 
 const app = express();
 
 app.use("/api/v1/book_sale", appRoute);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`server listening on port ${PORT}`);
-});
+module.exports = app;
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="container">
-    <div class="row mt-5">
-      <div class="col-md-6 col-sm-12 text-center">
-        <h1 class="display-3 mt-3 pt-3">Beat By God</h1>
-        <p class="lead">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum,
-          non.
-        </p>
-        <button class="btn btn-primary btn-large mt-2">Buy Book</button>
-      </div>
+    <div v-for="book in books" :key="book.id">
+      <div class="row mt-5">
+        <div class="col-md-6 col-sm-12 text-center">
+          <h1 class="display-3 mt-3 pt-3">{{ book.name }}</h1>
+          <p class="lead">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Voluptatum, non.
+          </p>
+          <button class="btn btn-primary btn-large mt-2" @click="buyBook">
+            Buy Book
+          </button>
+        </div>
 
-      <div class="col-md-6 col-sm-12 text-center">
-        <img src="img/images.jpg" alt="book" />
+        <div class="col-md-6 col-sm-12 text-center">
+          <img :src="`img/${book.img}`" alt="book" />
+        </div>
       </div>
     </div>
   </div>
@@ -19,6 +23,14 @@
 
 <script>
 export default {
-  name: "Home"
+  name: "Home",
+  data() {
+    return {
+      books: [{ name: "Beat by God", img: "images.jpg", id: 123 }]
+    };
+  },
+  methods: {
+    buyBook() {}
+  }
 };
 </script>
